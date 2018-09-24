fc311f8b266a9c8c8ee8b75535a6e106bd388355
Brian McGuirk <brian.mcguirk@doit.ri.gov>
2018-09-24 15:31:53 -0400

    Initial commit of agencies datamodel.

--- agencies/admin.py
+++ agencies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import FederalAgency, StateAgency, SubRecipient
+
+admin.site.register(FederalAgency)
+admin.site.register(StateAgency)
+admin.site.register(SubRecipient)
--- agencies/models.py
+++ agencies/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+# TODO: Handle addresses as separate model.
+
+class Organization(models.Model):
+    """
+    Macro Organization class, from which agencies will inherit.   
+    """
+    org_name = models.CharField(max_length=128)
+
+    class Meta():
+        abstract = True
+
+    def __str__(self):
+        return self.org_name[:50]
+
+
+class FederalAgency(Organization):
+    """
+    TODO: Establish parent-agency hierarchy, logically based on federal_hierarchy_level.
+    Fields:
+        cgac_code = "Common Government-wide Accounting Classification" code.
+        federal_hierarchy_level = Federal Hierarchy Level, as determined by beta.SAM.gov.
+    """
+    HIERARCHY = (
+        ('department', 'Department/Ind. Agency'),
+        ('sub_tier', 'Sub-tier')
+    )
+
+    cgac_code = models.CharField(max_length=3)
+    federal_hierarchy_level = models.CharField(
+        max_length=10,
+        choices=HIERARCHY
+    )
+
+
+class StateAgency(Organization):
+    """
+    TODO: Establish parent-agency hierarchy.
+    """
+
+    agency_code = models.CharField(max_length=2)
+
+
+class SubRecipient(Organization):
+    """
+
+    """
