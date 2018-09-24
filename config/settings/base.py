558f534a46596d595894ef2480ea2997bcee8bb5
Brian McGuirk <brian.mcguirk@doit.ri.gov>
2018-09-24 13:35:19 -0400

    Startapp agencies.

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,6 +70,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'fac.users.apps.UsersAppConfig',
+    'agencies'
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
